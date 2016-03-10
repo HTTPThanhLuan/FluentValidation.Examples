b224bb8160ef95cdb2a5766b4a65ae4f14bb9198
Radek Maziarka <maziarka.radoslaw@outlook.com>
2016-03-10 18:15:36 +0100

    Add initial example.

---
+++ Console/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = new CultureInfo("pl-PL");
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
+
+            var validator = new UserValidator();
+
+            var usersToValidate = new User[]
+            {
+                new User() {Email = "user@gmail.com", Password = "123456", UserName = "user"},
+                new User() {Email = "user@gmail", Password = "123", UserName = ""}
+            };
+
+            foreach (var user in usersToValidate)
+            {
+                var result = validator.Validate(user);
+                WriteValidationResults(user, result);
+            }
+
+            System.Console.ReadKey();
+        }
+
+        static void WriteValidationResults(User user, ValidationResult result)
+        {
+            if (result.IsValid)
+            {
+                System.Console.WriteLine($"Walidacja dla użytkownika {user.UserName} powiodła się.");
+            }
+            else
+            {
+                System.Console.WriteLine($"Walidacja dla użytkownika {user.UserName} nie powiodła się.");
+                foreach (var error in result.Errors)
+                {
+                    System.Console.WriteLine(error.ErrorMessage);
+                }
+            }
+        }
+
+    }
+}
---
+++ Console/UserValidator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Console
+{
+    public class UserValidator: AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            this.RuleFor(r => r.UserName).NotEmpty().Length(0, 50);
+
+            this.RuleFor(r => r.Email).NotEmpty().EmailAddress().Length(0, 100);
+
+            this.RuleFor(r => r.Password).NotEmpty().Length(6, 50);
+        }
+    }
+}
