4002a556f76ae4c6c2d6935d330a3586b074f066
Radek Maziarka <maziarka.radoslaw@outlook.com>
2016-03-13 23:36:18 +0100

    Add web to example.

---
+++ Web/Models/AddressViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentValidation;
+using FluentValidation.Attributes;
+
+namespace Web.Models
+{
+    [Validator(typeof(AddressViewModelValidator))]
+    public class AddressViewModel
+    {
+        public string City { get; set; }
+
+        public string PostalCode { get; set; }
+    }
+
+    public class AddressViewModelValidator : AbstractValidator<AddressViewModel>
+    {
+        public AddressViewModelValidator()
+        {
+            this.RuleFor(r => r.City).NotEmpty().Length(0, 50);
+
+            this.RuleFor(r => r.PostalCode).NotEmpty().Matches("^[0-9]{2}-[0-9]{3}$");
+        }
+    }
+}
---
+++ Web/Models/UserViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentValidation;
+using FluentValidation.Attributes;
+
+namespace Web.Models
+{
+    [Validator(typeof(UserViewModelValidator))]
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public AddressViewModel Address { get; set; }
+    }
+
+    public class UserViewModelValidator : AbstractValidator<UserViewModel>
+    {
+        public UserViewModelValidator()
+        {
+            this.RuleFor(r => r.UserName).NotEmpty().Length(0, 50);
+
+            this.RuleFor(r => r.Email).NotEmpty().EmailAddress().Length(0, 100);
+
+            this.RuleFor(r => r.Password).NotEmpty().Length(6, 50);
+        }
+    }
+}
