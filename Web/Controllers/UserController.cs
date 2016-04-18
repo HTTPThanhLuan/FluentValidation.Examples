9efa2751fb5541a22b602515dfa72afe6b95d4f1
Radek Maziarka <maziarka.radoslaw@outlook.com>
2016-04-18 22:15:59 +0200

    Web Api with Fluent Validation.

---
+++ Web/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class UserController : ApiController
+    {
+        public HttpResponseMessage PostUser(UserViewModel user)
+        {
+            // create user
+
+            return this.Request.CreateResponse(HttpStatusCode.Created);
+        }
+    }
+}
---
+++ Web/ModelStateFilterAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Http.ModelBinding;
+
+namespace Web
+{
+public class ModelStateFilterAttribute : ActionFilterAttribute
+{
+    public override void OnActionExecuting(HttpActionContext actionContext)
+    {
+        if (!actionContext.ModelState.IsValid)
+        {
+            var errors = actionContext.ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage);
+
+            actionContext.Response = actionContext.Request.CreateResponse(
+                HttpStatusCode.BadRequest, errors);
+        }
+    }
+}
+}
